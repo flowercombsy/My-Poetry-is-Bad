b1c1e04ec363e03ac24d32571bb344fdb61f687b
flowercombsy <solventi@outlook.com>
2026-09-25 17:36:04 +0900

    Update stories.js

--- scripts/content/stories.js
+++ scripts/content/stories.js
@@ -2,20 +2,18 @@
 
 const stories = [
   {
-    id: "the-beginning-of-everything",
-    title: "The Beginning of Everything.",
+    id: "Act - 2",
+    title: "The Jester - Act 2",
     date: "2026",
-    preview: "As I run through the forest,",
-    body: `I run from the bandits that slayed my mother,
-    Nothing here is comforting, I am in pain`
+    preview: "Lorem Ipsum",
+    body: `Lorem Ipsum`
   },
   {
-    id: "will-i-be-able-to-eat-today",
-    title: "Will I be able to eat today?",
+    id: "act-1",
+    title: "The Jester - Act 1",
     date: "2026",
-    preview: "I have been for days without food,",
-    body: `I have been running for days without food,
-    I feel my legs start to weaken,
-    My life, is this what it amounts to?`
+    preview: "The traveling carnival is at town, their newest jester is at play",
+    body: `The traveling carnival is at town, their newest jester is at play
+    	"Oh?" The girl pondered about going to the newest circus`
   }
 ];
